53a55d6dfb6034e4b2e6f04e2cc72f5021ecf2d3
Potewo <potewo@protonmail.com>
2020-08-17 18:25:57 +0900

    You can resize max 700 px

--- main.js
+++ main.js
@@ -1,12 +1,53 @@
 function loadImage(obj) {
   var base64ImageTagsContainer = document.getElementById("base64ImageTagsContainer");
   var fileReader = new FileReader();
-  var base64 = "";
   fileReader.onload = (function() {
-    base64 = fileReader.result;
-    base64ImageTagsContainer.insertAdjacentHTML("beforeend", "<a onclick='copyText(this.innerHTML)' ><img src='" + base64 + "'></a>")
+    console.log(fileReader.result.width);
+    resized(fileReader.result, function(base64) {
+      base64ImageTagsContainer.insertAdjacentHTML("beforeend", "<a onclick='copyText(this.innerHTML)' ><img src='" + base64 + "'></a>")
+    });
   });
   fileReader.readAsDataURL(obj.files[0]);
+}
+
+function resized(base64data, callback) {
+  var b64data;
+  ImgB64Resize(base64data, 
+    function(img_b64) {
+      b64data = img_b64;
+      console.log(b64data);
+      callback(b64data);
+    }
+  );
+}
+
+function ImgB64Resize(imgB64_src, callback) {
+  var img_type = imgB64_src.substring(5, imgB64_src.indexOf(";"));
+  // Source Image
+  var img = new Image();
+  var resizedWidth = 0;
+  var resizedHeight = 0;
+  img.onload = function() {
+    // New Canvas
+    var canvas = document.createElement('canvas');
+    if(img.width > img.height) {
+      resizedWidth = 700;
+      resizedHeight = 700 * img.height / img.width;
+    } else {
+      resizedHeight = 700;
+      resizedWidth = 700 + img.width / img.height;
+    }
+    canvas.width = resizedWidth;
+    canvas.height = resizedHeight;
+    // Draw (Resize)
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0, resizedWidth, resizedHeight);
+    // Destination Image
+    var imgB64_dst = canvas.toDataURL(img_type);
+    // console.log(imgB64_dst);
+    callback(imgB64_dst);
+  };
+  img.src = imgB64_src;
 }
 
 function copyText(text) {
